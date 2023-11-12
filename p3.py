005b394872e80b4ab86e3991833750ac191d4ebd
X. Li <72299922+Gariscat@users.noreply.github.com>
2023-11-12 16:36:55 +0400

    Add files via upload

--- p3.py
+++ p3.py
@@ -14,7 +14,7 @@ from copy import deepcopy
 
 LOG_DIR = './logs'
 os.makedirs(LOG_DIR, exist_ok=True)
-BATCH_SIZE = 2048
+BATCH_SIZE = 1024
 SEED = 26
 torch.random.manual_seed(SEED)
 np.random.seed(SEED)
@@ -191,7 +191,7 @@ if __name__ == '__main__':
         ### test_y_pred = un_standardize(test_y_pred, y_mean, y_std)
         # error = mean_squared_error(test_y, test_y_pred, multioutput='raw_values')
         error = (test_y - test_y_pred) ** 2
-        print(error)
+
         # result = deepcopy(config)
         # result.update({'error_avg': np.mean(error), 'error_std': np.std(error)})
         # results.append(str(result)+'\n')
