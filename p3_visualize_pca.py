c5b9c5a5f1cdf63b98e7c08a945c2089c1cd547d
X. Li <72299922+Gariscat@users.noreply.github.com>
2023-11-12 16:11:56 +0400

    Add files via upload

---
+++ p3_visualize_pca.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.decomposition import PCA
+
+train_data = pd.read_csv('Reg_Train.txt', sep=' ', header=None)
+test_data = pd.read_csv('Reg_Test.txt', sep=' ', header=None)
+
+train_x = np.array(train_data.iloc[:, :-1])
+train_y = np.array(train_data.iloc[:, -1])
+test_x = np.array(train_data.iloc[:, :-1])
+test_y = np.array(train_data.iloc[:, -1])
+
+
+pca = PCA(n_components=2)
+
+pca.fit(train_x)
+w = pca.components_.T
+print(w.shape)
+
+tmp = train_x @ w
+Xs = tmp[:, 0]
+Ys = tmp[:, 1]
+Zs = train_y
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.scatter(Xs, Ys, Zs)
+plt.show()
+plt.close()
+
+
+tmp = test_x @ w
+Xs = tmp[:, 0]
+Ys = tmp[:, 1]
+Zs = test_y
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+ax.scatter(Xs, Ys, Zs)
+plt.show()
+plt.close()
